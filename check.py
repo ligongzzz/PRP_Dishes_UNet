92be0ad1e5f169f326566c83c3529e088106ac6a
ligongzzz <xiezhe20001128@163.com>
2020-01-27 22:59:59 +0800

    Update.

--- check.py
+++ check.py
@@ -6,48 +6,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torchvision
 import torchvision.transforms as transforms
+import os
+
+plt.rcParams['font.family'] = ['sans-serif']
+plt.rcParams['font.sans-serif'] = ['SimHei']
 
 # Hyper Parameters
 DEVICE = 'cuda'
 IMG_SIZE = 256
-OUT_FEATURES = 4
-
-# Set the input image.
-raw_img: Image.Image = Image.open('./img/ljcd2.jpg')
-
-# Transform.
-transformer = transforms.Compose([
-    transforms.Resize((IMG_SIZE, IMG_SIZE), Image.NEAREST),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])
-])
-input_img = transformer(raw_img).to(DEVICE).unsqueeze(0)
+OUT_FEATURES = 17
 
 # Load the net.
 net = UNet(3, OUT_FEATURES).to(DEVICE)
 net.load_state_dict(torch.load('./net_param.pkl'))
+print('Loaded the net successfully!')
 
-print('Loaded the image and the net successfully!')
+# Load the labels.
+labels = [i.replace('\n', '') for i in open(
+    './labels.txt', 'rt').readlines()]
+labels.remove(labels[0])
 
-# Start UNet.
-net.eval()
-with torch.no_grad():
-    pred = net(input_img).squeeze()
-    pred_img = torch.max(pred, 0)[1].to(
-        'cpu').detach().unsqueeze(0).type(torch.uint8)
-    print('Finished UNet.')
+for root, dirs, files in os.walk('./test_img'):
+    for f in files:
+        if not f.endswith('.jpg'):
+            continue
 
-    # Show the result.
-    transform_to_img = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((raw_img.height, raw_img.width), Image.NEAREST)
-    ])
-    pred_img = transform_to_img(pred_img)
+        # Set the input image.
+        raw_img: Image.Image = Image.open('./test_img/' + f)
 
-    plt.subplot(1, 2, 1)
-    plt.title('origin')
-    plt.imshow(raw_img)
-    plt.subplot(1, 2, 2)
-    plt.title('pred')
-    plt.imshow(pred_img)
-    plt.show()
+        # Transform.
+        transformer = transforms.Compose([
+            transforms.Resize((IMG_SIZE, IMG_SIZE), Image.NEAREST),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])
+        ])
+        input_img = transformer(raw_img).to(DEVICE).unsqueeze(0)
+
+        print('Loaded the image successfully!')
+
+        # Start UNet.
+        net.eval()
+        with torch.no_grad():
+            pred = net(input_img).squeeze()
+            pred_img = torch.max(pred, 0)[1].to(
+                'cpu').detach().unsqueeze(0).type(torch.uint8)
+            print('Finished UNet.')
+
+            menu_list = []
+            for i in range(1, OUT_FEATURES):
+                rate = torch.sum(pred_img == i).numpy() / IMG_SIZE ** 2
+                if rate >= 0.03:
+                    menu_list.append(labels[i])
+
+            print(menu_list)
+
+            # Show the result.
+            transform_to_img = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.Resize(
+                    (raw_img.height, raw_img.width), Image.NEAREST)
+            ])
+            pred_img = transform_to_img(pred_img)
+
+            plt.subplot(1, 2, 1)
+            plt.title('origin')
+            plt.imshow(raw_img)
+            plt.subplot(1, 2, 2)
+            plt.title('pred')
+            plt.imshow(pred_img)
+            plt.suptitle('识别结果：' + ' '.join(menu_list))
+            plt.show()
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import model.data_transforms as data_transforms
 import torch.utils.data as Data
+import math
+import paramiko
 
 
 # Hyper Parameters
@@ -17,14 +19,23 @@ DEVICE = 'cuda'
 LR = 1e-3
 MOMENTUM = 0.9
 EPOCH = 100
-OUT_FEATURES = 4
+OUT_FEATURES = 17
+L2_REGULATION = 1e-5
+
+# Connect to SSH Server
+print('Connecting to ssh server...')
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='120.27.241.203', port=22,
+            username='prp', password='00000000')
+print('Connected to server.')
 
 # Tensor to storage the images and masks.
 image_list = torch.empty([0, 3, IMG_SIZE, IMG_SIZE])
 mask_list = torch.empty([0, IMG_SIZE, IMG_SIZE])
 
 # Load the data.
-print('Loading the data...')
+print('Loading Images...')
 for root, dirs, files in os.walk('./img_voc/JPEGImages'):
     for f in files:
         # Open the files.
@@ -50,7 +61,7 @@ print('Data successfully loaded.')
 # Build the net
 net = UNet(3, OUT_FEATURES).cuda()
 optimizer = torch.optim.SGD(net.parameters(), lr=LR,
-                            momentum=MOMENTUM, weight_decay=1e-5)
+                            momentum=MOMENTUM, weight_decay=L2_REGULATION)
 loss_func = nn.CrossEntropyLoss().to(DEVICE)
 
 try:
@@ -62,8 +73,29 @@ except Exception as err:
 print('Net build finished.')
 
 
+def SSH_Write(s, mode=0):
+    global ssh
+    try:
+        if mode == 0:
+            ssh.exec_command(f'echo \'{s}\' >> log.txt')
+        else:
+            ssh.exec_command(f'echo \'{s}\' > log.txt')
+    except Exception:
+        print('Connection failed.')
+        try:
+            ssh.close()
+            ssh.connect(hostname='120.27.241.203', port=22,
+                        username='prp', password='00000000')
+        except Exception:
+            pass
+
+
 # Start training.
-print('Start training the net.')
+# SSH_Write('Connected to server.', 1)
+# SSH_Write('Start Execution...')
+TOTAL_STEP = math.ceil(image_list.shape[0] / BATCH_SIZE)
+print(
+    f'Start training the net. {EPOCH} epoches in total. {TOTAL_STEP} steps each epoch.')
 net.train()
 for epoch in range(EPOCH):
     total_loss = 0.0
@@ -77,18 +109,26 @@ for epoch in range(EPOCH):
         loss.backward()
         optimizer.step()
 
-        # plt.subplot(1, 3, 1)
-        # plt.title('origin')
-        # plt.imshow(data_transforms.transform_to_img(batchx[0].to('cpu')))
-        # plt.subplot(1, 3, 2)
-        # plt.title('label')
-        # plt.imshow(batchy[0].to('cpu').numpy())
-        # plt.subplot(1, 3, 3)
-        # plt.title('pred')
-        # plt.imshow(torch.max(pred[0], 0)[1].to('cpu').numpy())
-        # plt.show()
+        plt.subplot(1, 3, 1)
+        plt.title('origin')
+        plt.imshow(data_transforms.transform_to_img(batchx[0].to('cpu')))
+        plt.subplot(1, 3, 2)
+        plt.title('label')
+        plt.imshow(batchy[0].to('cpu').numpy())
+        plt.subplot(1, 3, 3)
+        plt.title('pred')
+        plt.imshow(torch.max(pred[0], 0)[1].to('cpu').numpy())
+        plt.show()
 
-        print(f'EPOCH:{epoch},STEP:{step},LOSS:{loss.item()}')
+        print(
+            f'EPOCH:{epoch},STEP:{step},LOSS:{loss.item()},EPOCH PROGRESS:{int(step/TOTAL_STEP*100)}%')
+        # SSH_Write(
+        #     f'EPOCH:{epoch},STEP:{step},LOSS:{loss.item()},EPOCH PROGRESS:{int(step/TOTAL_STEP*100)}%')
 
     torch.save(net.state_dict(), 'net_param.pkl')
-    print(f'Model saved! The total loss of epoch{epoch} is {total_loss}.')
+    print(
+        f'Model saved! The total loss of epoch{epoch} is {total_loss}. TOTAL PROGRESS:{int(epoch/EPOCH*100)}%')
+    # SSH_Write(
+    #     f'EPOCH:{epoch},STEP:{step},LOSS:{loss.item()},EPOCH PROGRESS:{int(step/TOTAL_STEP*100)}%')
+
+ssh.close()
